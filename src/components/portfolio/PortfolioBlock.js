0367bd2299bb9c5e55f33fba1ba9880a5be72eb2
Mg Sangkatoe <mgsangkatoe@Mgs-MacBook-Pro.local>
2023-08-24 07:04:58 +0630

    first upload

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -6,8 +6,9 @@ function PortfolioBlock(props) {
    const {image, live, source, title} = props;
    return (
       <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
-         <Box component={'img'} src={image} alt={'mockup'}/>
+         <Box width={470} borderRadius={2} component={'img'} src={image} alt={'mockup'}/>
          <h1 style={{fontSize: '2rem'}}>{title}</h1>
+         <p style={{ fontSize: '.8rem', color: 'pink', marginTop: '5px'}}>{props.loginInfo && props.loginInfo}</p>
          <Box className={'portfolio'} display={'flex'} flexDirection={'column'} gap={'0.5rem'}
               alignItems={'center'} fontSize={'1.5rem'} py={'2rem'}>
             <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
--- src/info/Info.js
+++ src/info/Info.js
@@ -1,9 +1,9 @@
-import self from "../img/self.png"
-import mock1 from "../img/mock1.png"
-import mock2 from "../img/mock2.png"
-import mock3 from "../img/mock3.png"
-import mock4 from "../img/mock4.png"
-import mock5 from "../img/mock5.png"
+import self from "../img/profile.png"
+import mock1 from "../img/project1.png"
+import mock2 from "../img/project2.png"
+import mock3 from "../img/project3.png"
+import mock4 from "../img/project4.png"
+// import mock5 from "../img/mock5.png"
 
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
@@ -23,10 +23,10 @@ of what each of the values mean.
  */
 
 export const info = {
-    firstName: "John",
-    lastName: "Smith",
+    firstName: "Zar Hlei Sang",
+    lastName: "",
     initials: "js", // the example uses first and last, but feel free to use three or more if you like.
-    position: "a Full Stack Developer",
+    position: "a Web Developer",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
     baseColor: colors[0],
@@ -37,15 +37,15 @@ export const info = {
         },
         {
             emoji: '🌎',
-            text: 'based in the US'
+            text: 'based in Myanmar'
         },
         {
             emoji: "💼",
-            text: "Systems Engineer at Google"
+            text: "Web Developer"
         },
         {
             emoji: "📧",
-            text: "johnsmith@gmail.com"
+            text: "zarhleisang96@gmail.com"
         }
     ],
     socials: [
@@ -55,40 +55,34 @@ export const info = {
             label: 'facebook'
         },
         {
-            link: "https://instagram.com",
-            icon: 'fa fa-instagram',
-            label: 'instagram'
-        },
-        {
-            link: "https://github.com",
+            link: "https://github.com/BenRoSang",
             icon: "fa fa-github",
             label: 'github'
         },
         {
-            link: "https://linkedin.com",
+            link: "https://linkedin.com/in/zar-hlei-sang-0aab93233",
             icon: "fa fa-linkedin",
             label: 'linkedin'
         },
-        {
-            link: "https://twitter.com",
-            icon: "fa fa-twitter",
-            label: 'twitter'
-        }
 // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
 // Just change the links so that they lead to your social profiles.
 
     ],
-    bio: "Hello! I'm John. I'm a systems engineer for Google. I studied CompSci at Harvard, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+    bio: "Hello! I'm Zar Hlei Sang. I'm a Web Developer for Google. I studied Technological University at Kalaymyo, I enjoy long walks on the beach, and I believe Technologies make life easier in daily life. You should hire me!",
     skills:
         {
-            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma'],
-            exposedTo: ['nodejs', 'python', 'adobe illustrator']
+            proficientWith: ['javascript', 'react', 'nextjs', 'PHP', 'git', 'github', 'bootstrap', 'Jquery', 'html5', 'css3', 'tailwindcss'],
+            exposedTo: ['nodejs', 'Laravel', 'Vue']
         }
     ,
     hobbies: [
         {
             label: 'reading',
             emoji: '📖'
+        },
+        {
+            label: 'Football',
+            emoji: '⚽'
         },
         {
             label: 'theater',
@@ -108,33 +102,28 @@ export const info = {
     portfolio: [ // This is where your portfolio projects will be detailed
         {
             title: "Project 1",
-            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            live: "https://chat-app-4e3a4.web.app", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
             source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
             image: mock1
         },
         {
             title: "Project 2",
-            live: "https://paytonpierce.dev",
+            live: "https://cryptoverse-blush.vercel.app",
             source: "https://github.com/paytonjewell",
             image: mock2
         },
         {
             title: "Project 3",
-            live: "https://paytonpierce.dev",
+            live: "https://ecommerce-front-seven-rho.vercel.app",
             source: "https://github.com/paytonjewell",
             image: mock3
         },
         {
             title: "Project 4",
-            live: "https://paytonpierce.dev",
+            live: "https://ecommerce-admin-amber-two.vercel.app",
             source: "https://github.com/paytonjewell",
-            image: mock4
+            image: mock4,
+            loginInfo: 'User Name: Admin, Password: nextauth'
         },
-        {
-            title: "Project 5",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock5
-        }
     ]
 }
